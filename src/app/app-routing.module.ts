51a20040d634d05a232348e507591079596530ad
muhammad-suleman-awan <mmsulemanawan@gmail.com>
2023-03-21 00:03:56 +0500

    adding routing and logo image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'register', pathMatch:'full'},
+  {path: 'register', component:CreateRegistrationComponent},
+  {path:'list', component:CreateRegistrationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
